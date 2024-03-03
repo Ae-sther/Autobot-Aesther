de3ce386757f4df3b5de7b90a5855f39b3a76760
Clifff <143686507+Cliffvincent@users.noreply.github.com>
2024-03-03 13:56:42 +0000

    Yuou

--- script/listbox.js
+++ script/listbox.js
@@ -1,87 +1,23 @@
 module.exports.config = {
- name: 'listbox',
- version: '1.0.0',
- credits: 'manhIT',
- role: 2,
- alliases: ['listbox'],
-cooldown: 0,
-hasPrefix: false,
-	usage: "",
-
+		name: "listbox",
+		version: "1.0.0",
+		credits: "Him",
+		role: 2,
+		description: "Lấy tên và id các nhóm chứa bot",
+		hasPrefix: false,
+	  aliases: ["all"]
+		usages: "allbox",
+		cooldown: 5
 };
 
-module.exports.handleReply = async function({ api, event, args, Threads, handleReply }) {
-
- if (parseInt(event.senderID) !== parseInt(handleReply.author)) return;
-
- var arg = event.body.split(" ");
- var idgr = handleReply.groupid[arg[1] - 1];
-
-
- switch (handleReply.type) {
-
-	case "reply":
-	 {
-		if (arg[0] == "ban" || arg[0] == "Ban") {
-		 const data = (await Threads.getData(idgr)).data || {};
-		 data.banned = 1;
-		 await Threads.setData(idgr, { data });
-		 global.data.threadBanned.set(parseInt(idgr), 1);
-		 api.sendMessage(`[${idgr}] It was successful!`, event.threadID, event.messageID);
-		 break;
-		}
-
-		if (arg[0] == "out" || arg[0] == "Out") {
-		 api.removeUserFromGroup(`${api.getCurrentUserID()}`, idgr);
-		 api.sendMessage("Out thread with id: " + idgr + "\n" + (await Threads.getData(idgr)).name, event.threadID, event.messageID);
-		 break;
-		}
-
-	 }
- }
-};
-
-
-module.exports.run = async function({ api, event, client }) {
- var inbox = await api.getThreadList(100, null, ['INBOX']);
- let list = [...inbox].filter(group => group.isSubscribed && group.isGroup);
-
- var listthread = [];
-
- //////////
-
-
- for (var groupInfo of list) {
-	let data = (await api.getThreadInfo(groupInfo.threadID));
-
-	listthread.push({
-	 id: groupInfo.threadID,
-	 name: groupInfo.name,
-	 sotv: data.userInfo.length,
-	});
-
- } //for
-
- var listbox = listthread.sort((a, b) => {
-	if (a.sotv > b.sotv) return -1;
-	if (a.sotv < b.sotv) return 1;
- });
-
- let msg = '',
-	i = 1;
- var groupid = [];
- for (var group of listbox) {
-	msg += `${i++}. ${group.name}\n🧩TID: ${group.id}\n🐸Member: ${group.sotv}\n\n`;
-	groupid.push(group.id);
- }
-
- api.sendMessage(msg + 'Reply "out" or "ban" the number of order to out or ban that thread!!', event.threadID, (e, data) =>
-	global.client.handleReply.push({
-	 name: this.config.name,
-	 author: event.senderID,
-	 messageID: data.messageID,
-	 groupid,
-	 type: 'reply'
-	})
- );
+module.exports.run = async function ({ api, event }) {
+		var num = 0, box = "";
+		api.getThreadList(100, null, ["INBOX"], (err, list) => {
+				list.forEach(info => {
+						if (info.isGroup && info.isSubscribed) {
+								box += `${num+=1}. ${info.name} - ${info.threadID}\n`;
+						}			
+				});
+				api.sendMessage(box, event.threadID, event.messageID);
+		});
 };
