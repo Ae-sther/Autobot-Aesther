3478c0f1abb60c952b0f2ea364b52d90f39a5a81
Hackay <166723122+Chuisunvoleur@users.noreply.github.com>
2024-04-12 08:12:37 +0000

    Update Développeur.js

--- script/Développeur.js
+++ script/Développeur.js
@@ -24,10 +24,10 @@ module.exports.run = async function({ api, event, args }) {
 		let seconds = Math.floor(time % 60);
 		const uptimeString = `${years > 0 ? `${years} years ` : ''}${months > 0 ? `${months} months ` : ''}${weeks > 0 ? `${weeks} weeks ` : ''}${days % 7 > 0 ? `${days % 7} days ` : ''}${hours > 0 ? `${hours} hours ` : ''}${minutes > 0 ? `${minutes} minutes ` : ''}${seconds} seconds`;
 
-		const prefix = "🤣";
-		const CREATORLINK = "https://www.facebook.com/profile.php?id=61551417118451";
-		const BOTCREATOR = "Metoushela walker";
-		const BOTNAME = "Megan AE";
+		const prefix = "😐";
+		const CREATORLINK = "https://www.facebook.com/share/jxU3yXTKyuY4Wx35/?mibextid=A7sQZp";
+		const BOTCREATOR = "𝘾𝙃𝘼𝙏𝘽𝙊𝙏 𝘾𝙊𝙈𝙈𝙐𝙉𝘼𝙐𝙏𝙔🚀";
+		const BOTNAME = "🌱𝘼𝙆𝘼𝙄🌱";
 		const FILESOWNER = "AE";
 		const juswa = moment.tz("Asia/Manila").format("『D/MM/YYYY』 【HH:mm:ss】");
 		const link = ["https://i.imgur.com/9LDVC57.mp4", "https://i.imgur.com/r7IxgiR.mp4", "https://i.imgur.com/J1jWubu.mp4", "https://i.imgur.com/DJylTiy.mp4", "https://i.imgur.com/v4mLGte.mp4", "https://i.imgur.com/uthREbe.mp4", "https://i.imgur.com/ee8fHna.mp4", "https://i.imgur.com/VffzOwS.mp4", "https://i.imgur.com/ci5nztg.mp4", "https://i.imgur.com/qHPeKDV.mp4", "https://i.imgur.com/Rkl5UmH.mp4",
