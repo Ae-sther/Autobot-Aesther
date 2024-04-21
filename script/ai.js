3326e6c8a70f5629ef061bfbd5f07fb08ac14328
Ae-sther <161462700+Ae-sther@users.noreply.github.com>
2024-04-21 21:25:29 +0300

    Update and rename ai2.js to ai.js

---
+++ script/ai.js
@@ -0,0 +1,47 @@
+const fonts = {
+  a: "a", b: "b", c: "c", d: "d", e: "e", f: "f", g: "g", h: "h",
+  i: "i", j: "j", k: "k", l: "l", m: "m", n: "n", o: "o", 
+  p: "p", q: "q", r: "r", s: "s", t: "t", u: "u", v: "v", 
+  w: "w", x: "x", y: "y", z: "z",
+  A: "𝗔", B: "𝗕", C: "𝗖", D: "𝗗", E: "𝗘", F: "𝗙", G: "𝗚", H: "𝗛", I: "𝗜",
+  J: "𝗝", K: "𝗞", L: "𝗟", M: "𝗠", N: "𝗡", O: "𝗢", P: "𝗣", Q: "𝗤", R: "𝗥",
+  S: "𝗦", T: "𝗧", U: "𝗨", V: "𝗩", W: "𝗪", X: "𝗫", Y: "𝗬", Z: "𝗭",
+};
+
+const axios = require('axios');
+
+module.exports.config = {
+  name: "ai",
+  version: 1.0,
+  credits: "aesther",//Api OtinXsandip
+  description: "AI",
+  hasPrefix: false,
+  usages: "{pn} [prompt]",
+  aliases: ["ai2", "bot"],
+  cooldown: 0,
+};
+
+module.exports.run = async function ({ api, event, args }) {
+  try {
+    const prompt = args.join(" ");
+    if (!prompt) {
+      await api.sendMessage("シƬHƐᗩ©:\n\n☁️ღゝ◡╹)ノ[📑] 𝗛𝗜 !!", event.threadID);
+      return;
+    }
+    const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+    const answer = response.data.answer;
+
+    let formattedAnswer = "";
+    for (let char of answer) {
+      if (fonts[char.toLowerCase()]) {
+        formattedAnswer += fonts[char.toLowerCase()];
+      } else {
+        formattedAnswer += char;
+      }
+    }
+
+    await api.sendMessage(`ღゝ◡╹)ノ[📑]:\n\n${formattedAnswer} ✦`, event.threadID);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
